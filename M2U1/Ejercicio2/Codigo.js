b464c33866c726d40d9fde0443bd553ca3eca72a
Lucila Romeo <lucila.romeo03@gmail.com>
2024-12-08 14:17:28 -0300

    solucionado

--- M2U1/Ejercicio2/Codigo.js
+++
@@ -1,8 +0,0 @@
-const textarea = document.getElementById('myTextarea');
-const charCount = document.getElementById('charCount');
-const maxChars = textarea.getAttribute('maxlength');
-
-textarea.addEventListener('input', () => {
-    const currentLength = textarea.value.length;
-    charCount.textContent = `${currentLength}/${maxChars} caracteres`;
-});
